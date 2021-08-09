2159231f38630b91e719d0c7d61a71191f4168c3
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-09 22:24:09 +1000

    Styling for the header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from "react-bootstrap";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+
 function App() {
   return (
     <>
-      <h1>Welcome to HiShop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to HiShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
