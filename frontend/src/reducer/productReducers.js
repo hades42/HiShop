6a464dedcd1884de3cc1b94e1f28e06c7d7a7f9c
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-12 00:27:51 +1000

    Fixing bug in transistion to other productDetail page. Return a new product value in productDetailReducer instead of spreading the state.

--- frontend/src/reducer/productReducers.js
+++ frontend/src/reducer/productReducers.js
@@ -2,7 +2,11 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
 } from "../constants/productConstants";
+
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
@@ -10,6 +14,22 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAIL_REQUEST:
+      return { loading: true, product: { reviews: [] } };
+    case PRODUCT_DETAIL_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAIL_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
