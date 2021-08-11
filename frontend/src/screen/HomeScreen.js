fa4fc56faa67481a7044ae4ffd2b1dfad41e19c2
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-11 20:23:38 +1000

    Using Redux for state management.

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,21 +1,37 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import Product from "../component/Product.js";
-import products from "../products";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <>
       <h1>Lastest Products</h1>
-      <Row>
-        {products.map((product) => {
-          return (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
-      </Row>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
     </>
   );
 };
--- frontend/src/screen/ProductScreen.js
+++ frontend/src/screen/ProductScreen.js
@@ -1,11 +1,20 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import axios from "axios";
 import Rating from "../component/Rating";
-import products from "../products";
 
 const ProductScreen = ({ match }) => {
-  console.log(match);
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+      setProducts(data);
+    };
+    fetchProduct();
+  }, [match]);
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
