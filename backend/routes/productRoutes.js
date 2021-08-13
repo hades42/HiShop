d375f3a6807c8187c565061f9bfb6178e3b6f033
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-13 21:57:21 +1000

    create new route for user API.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,11 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../models/productModel.js";
-import mongoose from "mongoose";
+import {
+  getProductsById,
+  getProducts,
+} from "../controller/productController.js";
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public (any one can hit this route)
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-// @desc    Fetch single products
-// @route   GET /api/products/:id
-// @access  Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route(`/:id`).get(getProductsById);
 
 export default router;
