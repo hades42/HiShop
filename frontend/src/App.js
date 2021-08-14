fb0666bc16fba5b87426c4bef79ce63ff0201ce6
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-14 21:21:08 +1000

    Commit remaning stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,23 @@ import Header from "./component/Header";
 import Footer from "./component/Footer";
 import HomeScreen from "./screen/HomeScreen";
 import ProductScreen from "./screen/ProductScreen";
+import CartScreen from "./screen/CartScreen";
+import LoginScreen from "./screen/LoginScreen";
+import RegisterScreen from "./screen/registerScreen";
+import ProfileScreen from "./screen/ProfileScreen";
+
 function App() {
   return (
     <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" component={HomeScreen} exact></Route>
+          <Route path="/login" component={LoginScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/profile" component={ProfileScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
         </Container>
       </main>
       <Footer />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,6 @@ import store from "./store";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,4 +15,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
