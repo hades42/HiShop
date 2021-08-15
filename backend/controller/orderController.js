2a3e8c4d40fb48a87b796de068de7394c33a7049
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-15 23:20:48 +1000

    Creating endpoint for getting order of each user.

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -77,4 +77,12 @@ const updateOrderToPaid = asynHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrders = asynHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,10 +4,13 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from "../controller/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+
 export default router;
