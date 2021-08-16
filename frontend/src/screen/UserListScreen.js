65fb64cdfd3ade29a82bd3bd3fd7726e9ab81dff
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-16 13:48:40 +1000

    Redirect user that are not admin accesses /user/profile

--- frontend/src/screen/UserListScreen.js
+++ frontend/src/screen/UserListScreen.js
@@ -6,14 +6,21 @@ import Message from "../component/Message";
 import Loader from "../component/Loader";
 import { listUsers } from "../actions/userActions";
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
-  console.log(userList);
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push("/login");
+    }
+  }, [dispatch, history, userInfo]);
 
   const deleteHandler = (id) => {
     console.log("delete user");
