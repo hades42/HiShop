9e57b359867f54611e54ea46e9b0e868508c8af6
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-16 23:55:35 +1000

    Edit function on screen.

--- frontend/src/screen/UserEditScreen.js
+++ frontend/src/screen/UserEditScreen.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Message from "../component/Message";
 import Loader from "../component/Loader";
 import FormContainer from "../component/FormContainer";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUser } from "../actions/userActions";
+import { USER_UPDATE_RESET } from "../constants/userConstant";
 
 const UserEditScreen = ({ match, history, location }) => {
   const userId = match.params.id;
@@ -19,18 +20,31 @@ const UserEditScreen = ({ match, history, location }) => {
   const userDetail = useSelector((state) => state.userDetail);
   const { loading, error, user } = userDetail;
 
+  const userUpdate = useSelector((state) => state.userUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate;
+
   useEffect(() => {
-    if (!user.name || user._id !== userId) {
-      dispatch(getUserDetails(userId));
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET });
+      history.push("/admin/userlist");
     } else {
-      setName(user.name);
-      setEmail(user.email);
-      setIsAdmin(user.isAdmin);
+      if (!user.name || user._id !== userId) {
+        dispatch(getUserDetails(userId));
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+        setIsAdmin(user.isAdmin);
+      }
     }
-  }, [dispatch, user, userId]);
+  }, [dispatch, user, userId, successUpdate, history]);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }));
   };
   return (
     <>
@@ -39,6 +53,8 @@ const UserEditScreen = ({ match, history, location }) => {
       </Link>
       <FormContainer>
         <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
         {loading ? (
           <Loader></Loader>
         ) : error ? (
