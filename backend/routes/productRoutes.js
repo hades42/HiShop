e9f084d85f67bd2ea25f18e6b5252eb0c1f6073a
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-17 14:24:15 +1000

    Adding endpoint for update and create new product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,16 @@ import {
   getProductsById,
   getProducts,
   deleteProduct,
+  updatedProduct,
+  createProduct,
 } from "../controller/productController.js";
 import { protect, adminProtect } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(protect, adminProtect, createProduct);
 router
   .route(`/:id`)
   .get(getProductsById)
-  .delete(protect, adminProtect, deleteProduct);
+  .delete(protect, adminProtect, deleteProduct)
+  .put(protect, adminProtect, updatedProduct);
 
 export default router;
