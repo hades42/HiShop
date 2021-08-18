1e6a8d4477f91f05846780000237baf790c55edc
hades42 <nguyenvannguyen.oc@gmail.com>
2021-08-19 00:38:46 +1000

    Searching product with regex

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -5,7 +5,16 @@ import Product from "../models/productModel.js";
 // @route   GET /api/products
 // @access  Public (any one can hit this route)
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const products = await Product.find({ ...keyword });
   res.json(products);
 });
 
